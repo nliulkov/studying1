33b19791082c6b032ae49374e971261f6a4a2606
nliulkov <liulkovn@gmail.com>
2018-10-16 22:00:33 +0300

    functions
    I've found out what is functions and how to use and call them

--- sandbox/src/main/java/ua/nickl/firstRound/first.java
+++ sandbox/src/main/java/ua/nickl/firstRound/first.java
@@ -4,7 +4,28 @@ public class first {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello, World!!!");
+    talk("Vasja");
+    talk("Sashko");
+    talk("Yuliia");
+
+    double a = 5;
+    System.out.println("Площадь квадрата со стороной " + a + " = " + kvadrat(a));
+
+    double storona1 = 7;
+    double storona2 = 9;
+    System.out.println("Площадь прямоугольника со сторонами " + storona1 + " и " + storona2 + " = " + premoj(storona1, storona2));
 
   }
+    public static void talk(String g) {
+    System.out.println("Hello, " + g + "!!!");
+    }
+
+    public static double kvadrat(double l) {
+
+     return l * l;
+    }
+
+    public static double premoj(double a, double b) {
+      return a * b;
+    }
 }
